c35defcfff03dd2127ae8f7142b1fa5b1b3c1da2
mamani828 <manipers71@gmail.com>
2025-12-08 00:40:41 -0800

    end of night

--- main.py
+++ main.py
@@ -214,8 +214,8 @@ if __name__ == "__main__":
                             joint_indices=[0,1,2],
                             max_iter=5000,
                             step_size=0.5,
-                            alpha=10.0,
-                            d_safe=0.05)
+                            alpha=50.0,
+                            d_safe=0.12)
         path_segment = rrt_planner.plan() #change to plan2() for RRT*, it runs at max iteration so it will take a bit but will give great paths
         # rrt_planner = RRT(q_start, q_goal, arm_id, collision_ids, joint_limits,
         #                     max_iter=5000,
